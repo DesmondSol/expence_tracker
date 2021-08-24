bc92738d0c9f3b1264cd851ae8f2f5c4f5ec85cb
DesmondSol <soltig66@gmail.com>
2021-08-24 04:46:05 +0300

    little change

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Demo'),
-      ),
-      body: Center(
-        child: Text('widget playground'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Card(
+              child: Container(
+                  color: Colors.blue, width: 100, child: Text('CHART')),
+              elevation: 4,
+            ),
+            Card(
+              child: Text('List of TX'),
+            )
+          ],
+        ));
   }
 }
