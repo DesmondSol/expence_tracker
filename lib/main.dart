5b9a4f6c559a1542829cd7566ed4252b6dd2c17b
DesmondSol <soltig66@gmail.com>
2021-08-25 13:39:10 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,18 @@ class MyApp extends StatelessWidget {
               title: Text('Flutter Demo'),
             ),
             body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Card(
-                  child: Text('CHART'),
-                  // child: Container(  one way is wrapping the child the other is wrapping the parent
-                  //     color: Colors.blue,
-                  //     width: double.infinity,
-                  //     child: Text('CHART')),
-                  elevation: 4,
+                Container(
+                  width: double.infinity,
+                  child: Card(
+                    child: Text('CHART'),
+                    color: Colors.blue,
+                    // child: Container(  one way is wrapping the child the other is wrapping the parent
+                    //     width: double.infinity,
+                    //     child: Text('CHART')),
+                    elevation: 4,
+                  ),
                 ),
                 Card(
                   child: Text('List of TX'),
