0d11d1ac61232874c459813ce89a86e32fdfa2c2
DesmondSol <soltig66@gmail.com>
2021-08-26 22:44:54 +0300

    app bar button not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,58 @@
-import './widgets/user_transaction.dart';
+import './widgets/transaction_list.dart';
+
+import './widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
+
+import 'models/transaction.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // late String ttlInput;
   // late String amntInput;
   // or just use a built in method for value getting with byilt in feature
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   final _messangerKey = GlobalKey<ScaffoldMessengerState>();
+
+  final List<Transaction> _userTran = [
+    Transaction(
+        id: '1', title: 'New shoes', amount: 49.9, date: DateTime.now()),
+    Transaction(
+        id: '2', title: 'New slippers', amount: 9.9, date: DateTime.now()),
+    Transaction(
+        id: '3', title: 'New phone', amount: 149.9, date: DateTime.now())
+  ];
+
+  void _addNewTransa(String txtitle, double txamount) {
+    int b = int.parse(_userTran.last.id) + 1;
+    final newTx = Transaction(
+        id: b.toString(),
+        title: txtitle,
+        amount: txamount,
+        date: DateTime.now());
+
+    setState(() {
+      _userTran.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext context) {
+    showModalBottomSheet<void>(
+        context: context,
+        builder: (BuildContext context) {
+          // context:
+          // context;
+          // builder:
+          // (_) {};
+          return NewTransaction(_addNewTransa);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,60 +64,65 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: Scaffold(
-          appBar: AppBar(
-            //actionsIconTheme: IconThemeData(color: Colors.white10, size: 36),
-            backgroundColor: Colors.teal,
-            actions: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.add),
-                tooltip: 'Add',
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: const Icon(Icons.account_box_outlined),
-                tooltip: 'About creators',
-                onPressed: () {
-                  _messangerKey.currentState!.showSnackBar(const SnackBar(
-                    content: Text(
-                      'made by Solomon T \n contact me: soltig66@gmail.com',
-                      textAlign: TextAlign.center,
+            appBar: AppBar(
+              //actionsIconTheme: IconThemeData(color: Colors.white10, size: 36),
+              backgroundColor: Colors.teal,
+              actions: <Widget>[
+                IconButton(
+                    icon: const Icon(Icons.add),
+                    tooltip: 'Add',
+                    onPressed: () {
+                      _startAddNewTransaction(context);
+                    }
+                    //     _startAddNewTransaction(co): Builder(
+                    // builder: (context) => _startAddNewTransaction(context)
                     ),
-                    backgroundColor: Colors.teal,
-                    //      duration: Duration(milliseconds: 200),
-                  ));
-                },
-              ),
-            ],
-            leading: Icon(Icons.access_time_sharp),
-            leadingWidth: 70,
-            title: const Text('Expense Planner'),
-            centerTitle: true,
-          ),
-          body: SingleChildScrollView(
-              //for full screen scroll
-              child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                child: Card(
-                  child: Text('CHART'),
-                  color: Colors.blue,
-                  // child: Container(  one way is wrapping the child the other is wrapping the parent
-                  //     width: double.infinity,
-                  //     child: Text('CHART')),
-                  elevation: 4,
+                IconButton(
+                  icon: const Icon(Icons.account_box_outlined),
+                  tooltip: 'About creators',
+                  onPressed: () {
+                    _messangerKey.currentState!.showSnackBar(const SnackBar(
+                      content: Text(
+                        'made by Solomon T \n contact me: soltig66@gmail.com',
+                        textAlign: TextAlign.center,
+                      ),
+                      backgroundColor: Colors.teal,
+                      //      duration: Duration(milliseconds: 200),
+                    ));
+                  },
                 ),
+              ],
+              leading: Icon(Icons.access_time_sharp),
+              leadingWidth: 70,
+              title: const Text('Expense Planner'),
+              centerTitle: true,
+            ),
+            body: SingleChildScrollView(
+                //for full screen scroll
+                child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  child: Card(
+                    child: Text('CHART'),
+                    color: Colors.blue,
+                    // child: Container(  one way is wrapping the child the other is wrapping the parent
+                    //     width: double.infinity,
+                    //     child: Text('CHART')),
+                    elevation: 4,
+                  ),
+                ),
+                TransactionList(_userTran)
+              ],
+            )),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: Builder(
+              builder: (context) => FloatingActionButton(
+                onPressed: () => _startAddNewTransaction(context),
+                child: Icon(Icons.add),
               ),
-              UserTransactions()
-            ],
-          )),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerFloat,
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: Icon(Icons.add),
-          ),
-        ));
+            )));
   }
 }
--- lib/widgets/user_transaction.dart
+++ lib/widgets/user_transaction.dart
@@ -1,46 +1,26 @@
-import './transaction_list.dart';
-import 'package:flutter/material.dart';
-import '../models/transaction.dart';
+// import './transaction_list.dart';
+// import 'package:flutter/material.dart';
+// import '../models/transaction.dart';
 
-import './new_transaction.dart';
+// import './new_transaction.dart';
 
-class UserTransactions extends StatefulWidget {
-  // const UserTransactions({Key? key}) : super(key: key);
+// class UserTransactions extends StatefulWidget {
+//   // const UserTransactions({Key? key}) : super(key: key);
 
-  @override
-  _UserTransactionsState createState() => _UserTransactionsState();
-}
+//   @override
+//   _UserTransactionsState createState() => _UserTransactionsState();
+// }
 
-class _UserTransactionsState extends State<UserTransactions> {
-  final List<Transaction> _userTran = [
-    Transaction(
-        id: '1', title: 'New shoes', amount: 49.9, date: DateTime.now()),
-    Transaction(
-        id: '2', title: 'New slippers', amount: 9.9, date: DateTime.now()),
-    Transaction(
-        id: '3', title: 'New phone', amount: 149.9, date: DateTime.now())
-  ];
+// class _UserTransactionsState extends State<UserTransactions> {
+ 
 
-  void _addNewTransa(String txtitle, double txamount) {
-    int b = int.parse(_userTran.last.id) + 1;
-    final newTx = Transaction(
-        id: b.toString(),
-        title: txtitle,
-        amount: txamount,
-        date: DateTime.now());
-
-    setState(() {
-      _userTran.add(newTx);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        NewTransaction(_addNewTransa),
-        TransactionList(_userTran)
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: <Widget>[
+//         NewTransaction(_addNewTransa),
+//         TransactionList(_userTran)
+//       ],
+//     );
+//   }
+// }
