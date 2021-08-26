62d1137e44ea73121890e207d0a0e202fabb0ed4
DesmondSol <soltig66@gmail.com>
2021-08-26 19:28:56 +0300

    some exceptions added on input and out put

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -8,6 +8,22 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addtx);
 
+  void submitData() {
+    if (titleController.text.isEmpty || amountContoller.text.isEmpty) {
+      print('please enter values');
+      return;
+    }
+    final enteredTitle = titleController.text;
+    final enteredAmnt = double.parse(amountContoller.text);
+    // addtx(titleController.text,  // another way of putting it
+    // double.parse(amountContoller.text)
+    if (enteredAmnt <= 0) {
+      print('please enter realistic values');
+      return;
+    }
+    addtx(enteredTitle, enteredAmnt);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -19,21 +35,19 @@ class NewTransaction extends StatelessWidget {
             TextField(
               controller: titleController,
               //     onChanged: (val) => ttlInput = val,
+              onSubmitted: (_) => submitData(),
               decoration: InputDecoration(labelText: "Title"),
             ),
             TextField(
               controller: amountContoller,
-              //      onChanged: (val) => amntInput = val,
+              onSubmitted: (_) => submitData(),
+
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              //      onChanged: (val) => amntInput = val,  is an alternative to thr controller above
               decoration: InputDecoration(labelText: "Amount"),
             ),
             TextButton(
-                onPressed: () {
-                  addtx(
-                      titleController.text, double.parse(amountContoller.text));
-
-                  print(titleController.text);
-                  print(amountContoller.text);
-                },
+                onPressed: submitData,
                 child: Text(
                   'Add Transactions',
                   style: TextStyle(color: Colors.teal),
