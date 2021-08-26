30f75389f37fe395f8d070f6921c7678d5d4002d
DesmondSol <soltig66@gmail.com>
2021-08-26 17:12:10 +0300

    splitting app first version

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,15 @@
-import 'package:expence_tracker/transaction.dart';
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1', title: 'New shoes', amount: 49.9, date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'New slippers', amount: 9.9, date: DateTime.now()),
-    Transaction(
-        id: 't4', title: 'New phone', amount: 149.9, date: DateTime.now())
-  ];
-
   // late String ttlInput;
   // late String amntInput;
   // or just use a built in method for value getting with byilt in feature
-  final titleController = TextEditingController();
-  final amountContoller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -47,75 +36,6 @@ class MyApp extends StatelessWidget {
                     elevation: 4,
                   ),
                 ),
-                Card(
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: <Widget>[
-                        TextField(
-                          controller: titleController,
-                          //     onChanged: (val) => ttlInput = val,
-                          decoration: InputDecoration(labelText: "Title"),
-                        ),
-                        TextField(
-                          controller: amountContoller,
-                          //      onChanged: (val) => amntInput = val,
-                          decoration: InputDecoration(labelText: "Amount"),
-                        ),
-                        TextButton(
-                            onPressed: () {
-                              print(titleController.text);
-                              print(amountContoller.text);
-                            },
-                            child: Text(
-                              'Add Transactions',
-                              style: TextStyle(color: Colors.teal),
-                            ))
-                      ],
-                    ),
-                  ),
-                ),
-                Column(
-                  children: transactions.map((tx) {
-                    return Card(
-                        child: Row(
-                      children: <Widget>[
-                        Container(
-                          margin: EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 20),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.teal, width: 2)),
-                          child: Text(
-                            '\$${tx.amount}',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.teal),
-                          ),
-                          padding: EdgeInsets.all(10),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              tx.title,
-                              style: TextStyle(
-                                  color: Colors.teal[700],
-                                  fontSize: 18 - 2,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              DateFormat.yMMMd().format(
-                                  tx.date), //'yyyy-MM-dd date constructor
-                              style: TextStyle(color: Colors.blueGrey),
-                            )
-                          ],
-                        )
-                      ],
-                    ));
-                  }).toList(),
-                )
               ],
             )));
   }
