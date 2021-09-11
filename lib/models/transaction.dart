f754c4caa153b50f6a14aafcdd4f63219ab8cd9a
Sol Tig <51263712+DesmondSol@users.noreply.github.com>
2021-09-11 22:43:22 +0300

    hive dependency and model updated

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,7 +1,17 @@
-class Transaction {
+import 'package:hive/hive.dart';
+
+part 'transaction.g.dart';
+
+
+@HiveType(typeId: 0)
+class Transaction extends HiveObject{
+  @HiveField(0)
   String id;
+  @HiveField(1)
   String title;
+  @HiveField(2)
   double amount;
+  @HiveField(3)
   DateTime date;
 
   Transaction(
